0dfec61ca70eb11c665056de4d00e3dfb2047148
doctorwithpepsi <danylo.sukach@etu.unicaen.fr>
2026-03-10 17:37:26 +0100

    ajoute des fichiers de listener

---
+++ livraison/src/Controler/EcouteurModel.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tp3;
+
+/**
+ *
+ * @author sukach251
+ */
+public interface EcouteurModel {
+    void ModeleMisAJour(Object source);
+    
+}
